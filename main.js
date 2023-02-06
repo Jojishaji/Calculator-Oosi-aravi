a4ec351c5eb12c8c12fdc3ea6f4e8c83bcbb502c
Jojishaji <jojishaji123@gmail.com>
2023-02-05 21:48:20 -0930

    calculator-oosi

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+// To display content inside the calculator display
+
+function display(content) {
+    calculatorScreen.value += content
+}
+
+function allClear() {
+    calculatorScreen.value = "";
+}
+
+function evalE() {
+    calculatorScreen.value = eval(calculatorScreen.value);
+}
+
+function backSpace() {
+    calculatorScreen.value = calculatorScreen.value.slice(0,-1)
+}
---
+++ new.js
@@ -0,0 +1,5 @@
+function evalE() {
+    var sum = calculatorScreen.value
+    result = eval(sum)
+    calculatorScreen.value = result
+}
